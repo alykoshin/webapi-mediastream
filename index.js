98828abe07893b5c12432940c531f7b1fceda668
Alexander <alykoshin@gmail.com>
2015-12-15 09:39:00 +0300

    fixed deps

--- index.js
+++ index.js
@@ -24,15 +24,7 @@ var WebapiMediaStream = function( stream ) {
 
   // Public properties
 
-  /**
-   * @memberOf WebapiMediaStream
-   * @type {MediaStream}
-   */
   self.stream = stream;
-
-  self.muted        = false;
-  self.videoEnabled = true;
-  self.audioEnabled = true;
 
   // Stream event handlers
 
@@ -60,59 +52,6 @@ var WebapiMediaStream = function( stream ) {
     debug.log('WebapiMediaStream.stream.onremovetrack(): evt:', evt);
     self.emit('_removetrack', evt);
   };
-
-  // Public methods - Stream control
-
-  self.stop = function() {
-    debug.log('WebapiMediaStream.stop()');
-    self.emit('stop');
-    stream.stop();
-  };
-
-  self.hasAudio = function() {
-    return self.stream.getAudioTracks().length > 0;
-  };
-
-  self.setAudioEnabled = function(value) {
-    debug.debug('WebapiMediaStream.setAudioEnabled('+value+')');
-    self.audioEnabled = value;
-    var audioTracks = self.stream.getAudioTracks();
-    for ( var i = 0, l = audioTracks.length; i < l; i++ ) {
-      audioTracks[i].enabled = value;
-    }
-  };
-
-  self.toggleAudioEnabled = function() {
-    debug.debug('WebapiMediaStream.toggleAudioEnabled()');
-    self.setAudioEnabled( ! self.audioEnabled );
-  };
-
-  self.hasVideo = function() {
-    return self.stream.getVideoTracks().length > 0;
-  };
-
-  self.setVideoEnabled = function(value) {
-    // Muting for screencast is disabled
-    // https://code.google.com/p/chromium/codesearch#chromium/src/third_party/libjingle/source/talk/media/webrtc/webrtcvideoengine.cc&q=Disable%20muting%20for%20screencast.&sq=package:chromium&type=cs&l=3232
-    //
-    debug.debug('WebapiMediaStream.setVideoEnabled('+value+')');
-    self.videoEnabled = value;
-    var tracks = self.stream.getVideoTracks();
-    for ( var i = 0, l = tracks.length; i < l; i++ ) {
-      tracks[i].enabled = value;
-    }
-  };
-
-  self.toggleVideoEnabled = function() {
-    // Muting for screencast is disabled
-    // https://code.google.com/p/chromium/codesearch#chromium/src/third_party/libjingle/source/talk/media/webrtc/webrtcvideoengine.cc&q=Disable%20muting%20for%20screencast.&sq=package:chromium&type=cs&l=3232
-    debug.debug('WebapiMediaStream.toggleVideoEnabled()');
-    self.setVideoEnabled( ! self.videoEnabled );
-  };
-
-  self.setAudioEnabled(self.audioEnabled);
-
-  self.setVideoEnabled(self.videoEnabled);
 
   //
 
